0f62036e1f8446308a1ea5a8200729fc96d7b94a
Ankita H <ankita+snowball@5x.co>
2025-04-30 16:22:38 +0000

    commit

--- models/Advanced/int_supplier_diversity_analysis.sql
+++
@@ -1,8 +0,0 @@
-WITH supplier_diversity AS (
-    SELECT
-        s_nationkey,
-        COUNT(DISTINCT s_suppkey) AS unique_suppliers
-    FROM {{ ref('stg_supplier') }}
-    GROUP BY s_nationkey
-)
-SELECT * FROM supplier_diversity
